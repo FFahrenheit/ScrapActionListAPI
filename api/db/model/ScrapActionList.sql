07f70e1627a2f878694a172a110c9ef3706316e5
Iván L. Murillo <diskman199@gmail.com>
2021-11-24 10:09:01 -0600

    Agregado diagrama de tablas y sentencias SQL

--- api/db/model/ScrapActionList.sql
+++ api/db/model/ScrapActionList.sql
@@ -40,7 +40,8 @@ CREATE TABLE issue(
     part VARCHAR(40) NOT NULL,
     originator VARCHAR(30) NOT NULL,
     details TEXT NOT NULL DEFAULT '',
-    evaluation DATE DEFAULT CURRENT_TIMESTAMP
+    evaluation DATE DEFAULT CURRENT_TIMESTAMP,
+    type VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE action(
@@ -54,6 +55,71 @@ CREATE TABLE action(
     responsible VARCHAR(30) NOT NULL,
     department INT NOT NULL
 );
+
+CREATE TABLE team(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    position VARCHAR(30) DEFAULT 'participant',
+    member VARCHAR(30) NOT NULL,
+    issue INT NOT NULL
+);
+
+CREATE TABLE evidence(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    description VARCHAR(120) DEFAULT 'evidence',
+    filename VARCHAR(255),
+    date DATE DEFAULT CURRENT_TIMESTAMP,
+    issue INT NOT NULL,
+    author VARCHAR(30)
+);
+
+CREATE TABLE incident(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    car VARCHAR(30)
+    issued DATE DEFAULT CURRENT_TIMESTAMP,
+    contact VARCHAR(60)
+    email VARCHAR(60)
+    issue INT NOT NULL 
+);
+
+
+CREATE TABLE complication(
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    customerPN VARCHAR(30),
+    pnDescription VARCHAR(120),
+    failure VARCHAR(120),
+    whatIs VARCHAR(120),
+    whereIs VARCHAR(120),
+    whenIs VARCHAR(120),
+    whoIs VARCHAR(120),
+    whyIs VARCHAR(120),
+    much VARCHAR(120),
+    often VARCHAR(120),
+    description VARCHAR(240),
+    repeated VARCHAR(5),
+    finalAffected VARCHAR(5),
+    customerAffected VARCHAR(5),
+    issue INT
+);
+
+ALTER TABLE team
+ADD CONSTRAINT FK_team_member
+FOREIGN KEY (member) REFERENCES users(username);
+
+ALTER TABLE team
+ADD CONSTRAINT FK_team_issue
+FOREIGN KEY (issue) REFERENCES issue(id);
+
+ALTER TABLE evidence
+ADD CONSTRAINT FK_evidence_issue
+FOREIGN KEY (issue) REFERENCES issue(id);
+
+ALTER TABLE incident
+ADD CONSTRAINT FK_incident_issue
+FOREIGN KEY (issue) REFERENCES issue(id);
+
+ALTER TABLE complication
+ADD CONSTRAINT FK_complication_issue
+FOREIGN KEY (issue) REFERENCES issue(id);
 
 ALTER TABLE part
 ADD CONSTRAINT FK_part_client
